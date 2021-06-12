298a5da327757539da7282a34f94c13fca42d806
Aldo <aldo.pena.93@hotmail.com>
2021-06-12 13:23:47 +0200

    added backslash

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.use("auth", authRoutes);
+router.use("/auth", authRoutes);
 
 const protectedRoutes = require("./protected");
 
